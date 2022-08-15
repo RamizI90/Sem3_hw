93223ed82d2375153748c8de13c2c711eca0e043
Рамиз <ramiz.ilyazov@mail.ru>
2022-08-15 17:07:20 +0300

    Ex23

---
+++ Program23.cs
@@ -0,0 +1,19 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+string? numberstring = Console.ReadLine();
+int number = int.Parse(numberstring!);
+int index  = 1 ;
+
+while (number >= index )
+{
+    var result = Math.Pow(index, 3); 
+    index ++;
+    Console.WriteLine(result);
+}
+
+
